6b942af92c12e625d5590bd1fe05226b71dbab50
Rizal <s2708jac@gmail.com>
2025-12-07 14:44:54 +0700

    first commit

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TelemetryController;
 
 Route::post('/telemetry', [TelemetryController::class, 'store']);
-Route::get('/telemetry/latest', [TelemetryController::class, 'latest']); // web fetch status terbaru
+Route::get('/telemetry/latest', [TelemetryController::class, 'latest']);
