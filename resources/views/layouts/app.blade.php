2b394628d44d9dc16737e087f179a305ccba2942
Rizal <s2708jac@gmail.com>
2025-12-07 13:59:26 +0700

    first commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -151,7 +151,7 @@
         <header class="topbar">
             <div>
                 <div style="font-weight:700">Dashboard Sawah</div>
-                <div style="font-size:.75rem; color:var(--muted)">Rizal – Monitoring hama burung & tikus</div>
+                <div style="font-size:.75rem; color:var(--muted)">Rizal Monitoring hama burung & tikus</div>
             </div>
             <div style="font-size:.75rem; color:var(--muted)">{{ now()->format('d M Y H:i') }} WIB</div>
         </header>
