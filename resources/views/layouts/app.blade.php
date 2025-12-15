8bae9151810a3397412a0baf099d3dd0a73efa0f
Rizal <s2708jac@gmail.com>
2025-12-15 23:34:40 +0700

    first commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,6 @@
     <title>Monitoring Sawah IoT</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Font -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600,700" rel="stylesheet" />
 
